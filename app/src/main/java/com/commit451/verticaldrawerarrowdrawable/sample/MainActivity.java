ba02a90b927771a4e70d23ceb63b59e6d12398ae
John Carlson <jawnnypoo@gmail.com>
2015-12-08 00:25:34 -0600

    Add configuration for releases, and show more of an example of a top drawer

--- app/src/main/java/com/commit451/verticaldrawerarrowdrawable/sample/MainActivity.java
+++ app/src/main/java/com/commit451/verticaldrawerarrowdrawable/sample/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.commit451.verticaldrawerarrowdrawable.VerticalDrawerArrowDrawable;
 
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         final VerticalDrawerArrowDrawable drawerArrowDrawable = new VerticalDrawerArrowDrawable(this);
+        final ViewGroup topDrawer = (ViewGroup) findViewById(R.id.top_drawer);
+        final int toolbarHeight = getResources().getDimensionPixelSize(R.dimen.toolbar_size);
+        final int topDrawerSize = getResources().getDimensionPixelSize(R.dimen.top_drawer_size);
+        topDrawer.setY(-topDrawerSize + toolbarHeight);
 //        drawerArrowDrawable.setColor(Color.WHITE);
         //drawerArrowDrawable.setSpinEnabled(false);
         //drawerArrowDrawable.setDirection(VerticalDrawerArrowDrawable.ARROW_DIRECTION_DOWN);
@@ -29,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
                 if (drawerArrowDrawable.getProgress() == 0.0f) {
                     ObjectAnimator.ofObject(drawerArrowDrawable, "progress", new FloatEvaluator(), 0.0f, 1.0f)
                             .start();
+                    topDrawer.animate().y(0);
                 } else if (drawerArrowDrawable.getProgress() == 1.0f) {
                     ObjectAnimator.ofObject(drawerArrowDrawable, "progress", new FloatEvaluator(), 1.0f, 0.0f)
                             .start();
+                    topDrawer.animate().y(-topDrawerSize + toolbarHeight);
                 }
             }
         });
